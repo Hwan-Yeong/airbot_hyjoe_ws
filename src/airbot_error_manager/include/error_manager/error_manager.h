93efeb98889b76465738d92b6a7f983974cc1b0a
hyjoe <hyjoe@everybot.net>
2025-02-24 17:40:54 +0900

    ✅ error_manager 통신단 작업 완료

--- src/airbot_error_manager/include/error_manager/error_manager.h
+++ src/airbot_error_manager/include/error_manager/error_manager.h
@@ -24,10 +24,30 @@ public:
     ~ErrorManager();
 
 private:
-    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr scan_front_error_sub_;
-    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr scan_back_error_sub_;
-    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr scan_dirty_front_error_sub_;
-    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr scan_dirty_back_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr e_left_motor_stuck_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr e_right_motor_stuck_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr e_scan_dirty_front_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr e_scan_dirty_back_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr e_docking_station_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr e_charging_error_sub_;
+
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr f_battery_charging_overcurrent_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr f_battery_discharging_overcurrent_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr f_top_tof_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr f_camera_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr f_right_motor_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr f_left_motor_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr f_scan_front_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr f_scan_back_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr f_battery_overheat_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr f_bot_tof_error_sub_;
+
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr s_unreachable_goal_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr s_change_temporary_goal_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr s_fall_down_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr s_unable_to_docking_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr s_board_overheat_error_sub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr s_station_overheat_error_sub_;
 
     rclcpp::Publisher<robot_custom_msgs::msg::ErrorListArray>::SharedPtr error_list_pub_;
 
@@ -36,13 +56,35 @@ private:
     std::vector<std::shared_ptr<robot_custom_msgs::msg::ErrorList>> error_list_;
 
 private:
-    void scanFrontErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
-    void scanBackErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void leftMotorStuckErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void rightMotorStuckErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
     void scanDirtyFrontErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
     void scanDirtyBackErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void dockingStationErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void chargingErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+
+    void batChargingOverCurrentErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void batDischargingOverCurrentErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void topTofErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void cameraErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void rightMotorErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void leftMotorErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void scanFrontErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void scanBackErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void batOverHeatErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void botTofErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+
+    void unReachableGoalErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void changeTempGoalErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void fallDownErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void unableToDockingErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void boardOverHeatErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+    void stationOverHeatErrorCallback(const std_msgs::msg::Bool::SharedPtr msg);
+
     void updateErrorLists(int rank, std::string code);
     void publishErrorList();
     void addError(int rank, const std::string &error_code);
+    void printErrorList();
 };
 
 #endif // ERROR_MANAGER_HPP
