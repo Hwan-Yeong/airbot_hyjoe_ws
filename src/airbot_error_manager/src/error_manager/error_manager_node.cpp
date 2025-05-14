e72c608189fee1c81bbfa93eb3d8347c55823429
hyjoe <hyjoe@everybot.net>
2025-05-14 14:12:50 +0900

    ✅ localization faiil error 추가

--- src/airbot_error_manager/src/error_manager/error_manager_node.cpp
+++ src/airbot_error_manager/src/error_manager/error_manager_node.cpp
@@ -42,6 +42,7 @@ ErrorManagerNode::ErrorManagerNode()
 
     // 에러 발생 후 에러 리스트에서 관리하지 않을 에러 등록
     erase_after_pub_error_codes_.insert("S05");
+    erase_after_pub_error_codes_.insert("S06");
 
     error_list_pub_ = this->create_publisher<robot_custom_msgs::msg::ErrorListArray>("/error_list", 10);
 
